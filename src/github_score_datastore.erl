05c4bee2ab0b868b80325e38d43a6aff271eba1f
Micah Warren <null23544@gmail.com>
2021-08-04 12:49:36 -0500

    Implemented first draft of datastore.
    As expected, some tests weren't perfect, but those were fixed. They did
    catch some expectations and assumptions I missed.
    
    While the specs / docs may be a bit overkill for something so simple, it's
    a habit of mine, and given little time pressue I prefer to have them (and
    not need them) than lack them.

--- src/github_score_datastore.erl
+++ src/github_score_datastore.erl
@@ -21,24 +21,37 @@ start_link() ->
 	_Ets = ets:new(?MODULE, [public, named_table]),
 	ignore.
 
+%% @doc Given a user identifier and an amount, increase the datastore's track
+%% of that user's score by the given amount. If the user is not yet tracked,
+%% their score is set to the given amount.
+-spec increment_score(User :: term(), integer()) -> {ok, integer()}.
 increment_score(User, Amount) ->
-	throw(nyi).
+	N = ets:update_counter(?MODULE, User, {2, Amount}, {User, 0}),
+	{ok, N}.
 
+%% @doc Fetch the score for a given user identifier. Users that have yet to be
+%% tracked have a default score of 0.
+-spec get_score(User :: term()) -> {ok, integer()}.
 get_score(User) ->
-	throw(nyi).
+	try ets:lookup_element(?MODULE, User, 2) of
+		N -> {ok, N}
+	catch
+		error:badarg ->
+			{ok, 0}
+	end.
 
 -ifdef(TEST).
 
 simple_test_() ->
-	{setup, fun() ->
+	{setup, local, fun() ->
 		ignore = ?MODULE:start_link()
 	end,
 	[ ?_assertEqual({ok, 0}, ?MODULE:get_score(<<"no user">>))
-	, ?_assertEqual({ok, 1}, ?MODULE:incement_score(<<"user a">>))
+	, ?_assertEqual({ok, 1}, ?MODULE:increment_score(<<"user a">>, 1))
 	, ?_assertEqual({ok, 0}, ?MODULE:get_score(<<"no user">>))
-	, ?_assertEqual({ok, 5}, ?MODULE:increment_score(<<"user a">>))
+	, ?_assertEqual({ok, 5}, ?MODULE:increment_score(<<"user a">>, 4))
 	, ?_assertEqual({ok, 5}, ?MODULE:get_score(<<"user a">>))
-	, ?_assertEqual({ok, 27}, ?MODULE:increment_score(<<"user b">>))
+	, ?_assertEqual({ok, 27}, ?MODULE:increment_score(<<"user b">>, 27))
 	, ?_assertEqual({ok, 5}, ?MODULE:get_score(<<"user a">>))
 	] }.
 
