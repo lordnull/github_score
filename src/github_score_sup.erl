8bc6a1a9ef6df9b2da5dab83f3efde868471625c
Micah Warren <null23544@gmail.com>
2021-08-04 19:20:21 -0500

    Filled out the supervisor tree.
    This also links together the poller and the datastore.

--- src/github_score_sup.erl
+++ src/github_score_sup.erl
@@ -8,5 +8,25 @@ start_link() ->
 	supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 init([]) ->
-	Procs = [],
+	Procs =
+	[ #{ id => github_score_datastore ,
+		 start => {github_score_datastore, start_link, []} }
+	, #{ id => github_score_poller,
+		 start => {github_score_poller, start_link, ["https://api.github.com/events", default_score_fun()]} }
+	],
 	{ok, {{one_for_one, 1, 5}, Procs}}.
+
+default_score_fun() ->
+	fun(Events) ->
+		lists:map(fun increment_user/1, Events)
+	end.
+
+increment_user({User, EventName}) ->
+	Increment = get_increment(EventName),
+	github_score_datastore:increment_user(User, Increment).
+
+get_increment(<<"PushEvent">>) -> 5;
+get_increment(<<"PullRequestReviewCommentEvent">>) -> 4;
+get_increment(<<"WatchEvent">>) -> 3;
+get_increment(<<"CreateEvent">>) -> 2;
+get_increment(_) -> 1.
